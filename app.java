3a540c9d9ca17a65fc0e3125796961fbab735bab
iamglicious <gregory.vrede@ia-events.com>
2017-01-25 12:38:02 +0100

    add dostuff

--- app.java
+++ app.java
@@ -3,5 +3,9 @@ public class App{
 	public static void main(String[] args){
 		System.out.println("App Main");
 		System.out.println("Test 2");
+		doStuff();
+	}
+	private static void doStuff(){
+		System.out.println("App.DoStuff");
 	}
 }
